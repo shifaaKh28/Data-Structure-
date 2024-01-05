f6bb9cb29fa50381289db924d61d77e043f025c4
shifaa_Khatib <132808634+shifaaKh28@users.noreply.github.com>
2024-01-06 00:25:59 +0200

    Create  BinaryTree.java
    this class present binaryTree

---
+++ BinaryTree.java
@@ -0,0 +1,11 @@
+public class BinaryTree {
+    private final Node root;
+    public BinaryTree(Node root) {
+        this.root = root;
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+}
+
